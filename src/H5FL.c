b060944bab70bd8f06bf749087d587b13c959ed1
Quincey Koziol <quincey@koziol.cc>
2024-07-13 16:57:54 -0500

    Cleanup code a bit
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5FL.c
+++ src/H5FL.c
@@ -2911,14 +2911,14 @@ H5FL_get_free_list_sizes(size_t *reg_size, size_t *arr_size, size_t *blk_size, s
 
     /* Retrieve the amount of "factory" memory used */
     if (fac_size) {
+        H5FL_fac_gc_node_t *gc_fac_node; /* Pointer into the list of things to garbage collect */
+
 #ifdef H5_HAVE_CONCURRENCY
         if (H5FL_fac_gc_head.init) {
             /* Acquire the mutex protecting the list of lists */
             if (H5TS_dlftt_mutex_acquire(&H5FL_fac_gc_head.mutex) < 0)
                 HGOTO_ERROR(H5E_RESOURCE, H5E_CANTLOCK, FAIL, "can't lock list of list's mutex");
 #endif /* H5_HAVE_CONCURRENCY */
-
-            H5FL_fac_gc_node_t *gc_fac_node; /* Pointer into the list of things to garbage collect */
 
             /* Walk through all the free lists */
             gc_fac_node = H5FL_fac_gc_head.first;
