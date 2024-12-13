75ef3283bfc71d15bd7ae5de4feb1ec7eeb4cb81
Quincey Koziol <quincey@koziol.cc>
2024-12-13 17:12:03 -0600

    Fix parsing for trace script
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5Iint.c
+++ src/H5Iint.c
@@ -2404,11 +2404,11 @@ done:
  *-------------------------------------------------------------------------
  */
 herr_t
-H5I__id_info_release(H5I_id_info_t *
+H5I__id_info_release(H5I_id_info_t
 #ifndef H5_HAVE_CONCURRENCY
                          H5_ATTR_UNUSED
 #endif /* NDEBUG */
-                             info)
+                            *info)
 {
     herr_t ret_value = SUCCEED; /* Return value */
 
