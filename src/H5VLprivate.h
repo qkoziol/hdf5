8d83f084acc64636c21d75b10a110b88a71ecdd7
Quincey Koziol <quincey@koziol.cc>
2025-02-03 13:57:11 -0600

    Switch H5P_object_verify from ID to class type enum
    Also move dereference of DCPL up to API routines
    
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5VLprivate.h
+++ src/H5VLprivate.h
@@ -191,7 +191,7 @@ H5_DLL herr_t H5VL_attr_close(const H5VL_object_t *vol_obj, hid_t dxpl_id, void 
 /* Dataset functions */
 H5_DLL void  *H5VL_dataset_create(const H5VL_object_t *vol_obj, const H5VL_loc_params_t *loc_params,
                                   const char *name, hid_t lcpl_id, hid_t type_id, hid_t space_id,
-                                  hid_t dcpl_id, hid_t dapl_id, hid_t dxpl_id, void **req);
+                                  H5P_genplist_t *dcpl, hid_t dapl_id, hid_t dxpl_id, void **req);
 H5_DLL void  *H5VL_dataset_open(const H5VL_object_t *vol_obj, const H5VL_loc_params_t *loc_params,
                                 const char *name, hid_t dapl_id, hid_t dxpl_id, void **req);
 H5_DLL herr_t H5VL_dataset_read(size_t count, void *obj[], H5VL_connector_t *connector, hid_t mem_type_id[],
