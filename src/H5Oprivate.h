1f9e6459403639f01bbdca77fc78ebdf2950bbc4
Quincey Koziol <quincey@koziol.cc>
2025-02-03 14:04:43 -0600

    Switch from 'plist' -> 'dapl'
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5Oprivate.h
+++ src/H5Oprivate.h
@@ -982,7 +982,7 @@ H5_DLL herr_t H5O_msg_get_flags(const H5O_loc_t *loc, unsigned type_id, uint8_t 
 H5_DLL herr_t H5O_flush(H5O_loc_t *oloc, hid_t obj_id);
 H5_DLL herr_t H5O_flush_common(H5O_loc_t *oloc, hid_t obj_id);
 H5_DLL herr_t H5O_refresh_metadata(H5O_loc_t *oloc, hid_t oid);
-H5_DLL herr_t H5O_refresh_metadata_reopen(hid_t oid, H5P_genplist_t *apl_plist, H5G_loc_t *obj_loc,
+H5_DLL herr_t H5O_refresh_metadata_reopen(hid_t oid, H5P_genplist_t *apl, H5G_loc_t *obj_loc,
                                           H5VL_connector_t *connector, bool start_swmr);
 
 /* Object copying routines */
