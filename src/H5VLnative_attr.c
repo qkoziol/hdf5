87601a2d0007a9deaed47dc6376de0ab7ddc8833
Quincey Koziol <quincey@koziol.cc>
2025-03-13 12:23:26 -0500

    Fix warnings
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5VLnative_attr.c
+++ src/H5VLnative_attr.c
@@ -74,7 +74,7 @@
  */
 void *
 H5VL__native_attr_create(void *obj, const H5VL_loc_params_t *loc_params, const char *attr_name, hid_t type_id,
-                         hid_t space_id, hid_t acpl_id, hid_t aapl_id, hid_t dxpl_id,
+                         hid_t space_id, hid_t acpl_id, hid_t aapl_id, hid_t H5_ATTR_UNUSED dxpl_id,
                          void H5_ATTR_UNUSED **req)
 {
     H5P_genplist_t *plist;
