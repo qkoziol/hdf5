117b1775c7be83263f878c40ed15ad98060f0f60
Matt L <124107509+mattjala@users.noreply.github.com>
2025-06-20 11:02:55 -0500

    Delay dataset layout copy to DCPL (#5537)

--- src/H5Dlayout.c
+++ src/H5Dlayout.c
@@ -570,11 +570,11 @@ done:
 herr_t
 H5D__layout_oh_read(H5D_t *dataset)
 {
-    htri_t msg_exists;              /* Whether a particular type of message exists */
-    bool   pline_copied  = false;   /* Flag to indicate that dcpl_cache.pline's message was copied */
-    bool   layout_copied = false;   /* Flag to indicate that layout message was copied */
-    bool   efl_copied    = false;   /* Flag to indicate that the EFL message was copied */
-    herr_t ret_value     = SUCCEED; /* Return value */
+    htri_t msg_exists;                      /* Whether a particular type of message exists */
+    bool   pline_copied          = false;   /* Flag to indicate that dcpl_cache.pline's message was copied */
+    bool   layout_copied_to_dset = false;   /* Flag to indicate that layout message was copied */
+    bool   efl_copied            = false;   /* Flag to indicate that the EFL message was copied */
+    herr_t ret_value             = SUCCEED; /* Return value */
 
     FUNC_ENTER_PACKAGE
 
@@ -602,7 +602,7 @@ H5D__layout_oh_read(H5D_t *dataset)
      */
     if (NULL == H5O_msg_read(&(dataset->oloc), H5O_LAYOUT_ID, &(dataset->shared->layout)))
         HGOTO_ERROR(H5E_DATASET, H5E_CANTINIT, FAIL, "unable to read data layout message");
-    layout_copied = true;
+    layout_copied_to_dset = true;
 
     /* Check for external file list message (which might not exist) */
     if ((msg_exists = H5O_msg_exists(&(dataset->oloc), H5O_EFL_ID)) < 0)
@@ -629,10 +629,6 @@ H5D__layout_oh_read(H5D_t *dataset)
         (dataset->shared->layout.ops->init)(dataset->oloc.file, dataset) < 0)
         HGOTO_ERROR(H5E_DATASET, H5E_CANTINIT, FAIL, "unable to initialize layout information");
 
-    /* Adjust chunk dimensions to omit datatype size (in last dimension) for creation property */
-    if (H5D_CHUNKED == dataset->shared->layout.type)
-        dataset->shared->layout.u.chunk.ndims--;
-
     /* Copy layout to the DCPL */
     if (H5P_set(dataset->shared->dcpl, H5D_CRT_LAYOUT_NAME, &dataset->shared->layout) < 0)
         HGOTO_ERROR(H5E_DATASET, H5E_CANTSET, FAIL, "can't set layout");
@@ -647,7 +643,7 @@ done:
         if (pline_copied)
             if (H5O_msg_reset(H5O_PLINE_ID, &dataset->shared->dcpl_cache.pline) < 0)
                 HDONE_ERROR(H5E_DATASET, H5E_CANTRESET, FAIL, "unable to reset pipeline info");
-        if (layout_copied)
+        if (layout_copied_to_dset)
             if (H5O_msg_reset(H5O_LAYOUT_ID, &dataset->shared->layout) < 0)
                 HDONE_ERROR(H5E_DATASET, H5E_CANTRESET, FAIL, "unable to reset layout info");
         if (efl_copied)
@@ -682,12 +678,11 @@ H5D__layout_oh_write(const H5D_t *dataset, H5O_t *oh, unsigned update_flags)
     /* Check if the layout message has been added to the dataset's header */
     if ((msg_exists = H5O_msg_exists_oh(oh, H5O_LAYOUT_ID)) < 0)
         HGOTO_ERROR(H5E_DATASET, H5E_CANTGET, FAIL, "unable to check if layout message exists");
-    if (msg_exists) {
+    if (msg_exists)
         /* Write the layout message to the dataset's header */
         if (H5O_msg_write_oh(dataset->oloc.file, oh, H5O_LAYOUT_ID, 0, update_flags,
                              &dataset->shared->layout) < 0)
             HGOTO_ERROR(H5E_DATASET, H5E_WRITEERROR, FAIL, "unable to update layout message");
-    } /* end if */
 
 done:
     FUNC_LEAVE_NOAPI(ret_value)
