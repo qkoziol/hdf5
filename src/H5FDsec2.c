ba60fffb3cc5f856e9bcc210fe532c1b7860cd3b
Quincey Koziol <quincey@koziol.cc>
2026-01-05 17:51:01 -0600

    Comment out flush callback
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5FDsec2.c
+++ src/H5FDsec2.c
@@ -153,7 +153,8 @@ static const H5FD_class_t H5FD_sec2_g = {
     NULL,                  /* read_selection       */
     NULL,                  /* write_selection      */
 #ifdef H5_HAVE_UNISTD_H
-    H5FD__sec2_flush, /* flush                */
+    // H5FD__sec2_flush, /* flush                */
+    NULL, /* flush                */
 #else
     NULL, /* flush                */
 #endif
