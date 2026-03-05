12f52739382ffe32a30bb2988472d9e62eda53f0
Quincey Koziol <quincey@koziol.cc>
2026-03-04 18:25:10 -0600

    Update calling H5FDcmp_driver_cls()
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- java/hdf/hdf5lib/H5.java
+++ java/hdf/hdf5lib/H5.java
@@ -7626,8 +7626,11 @@ public class H5 implements java.io.Serializable {
      * @exception HDF5LibraryException
      *            Error from the HDF5 Library.
      **/
-    public synchronized static native boolean H5FDcmp_driver_cls(long drvr_id1, long drvr_id2)
-        throws HDF5LibraryException;
+    public static boolean H5FDcmp_driver_cls(long drvr_id1, long drvr_id2) throws HDF5LibraryException
+    {
+        boolean ret = org.hdfgroup.javahdf5.hdf5_h.H5FDcmp_driver_cls(drvr_id1, drvr_id2);
+        return ret;
+    }
 
     // /////// unimplemented ////////
     //  hid_t H5FDregister(const H5FD_class_t *cls);
