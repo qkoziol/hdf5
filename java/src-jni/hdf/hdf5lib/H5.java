0a5642b328c5266594af3985a0ee30e3cd1a4c18
Quincey Koziol <quincey@koziol.cc>
2026-03-06 17:35:08 -0600

    Fix typo
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- java/src-jni/hdf/hdf5lib/H5.java
+++ java/src-jni/hdf/hdf5lib/H5.java
@@ -5427,7 +5427,7 @@ public class H5 implements java.io.Serializable {
      *            Error from the HDF5 Library.
      **/
     public synchronized static native boolean H5FDcmp_driver_cls(long drvr_id1, long drvr_id2)
-        throws HDF5LibraryException;
+        throws HDF5LibraryException
     {
         boolean ret = _H5FDcmp_driver_cls(drvr_id1, drvr_id2);
         return ret;
